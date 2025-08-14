51a59bb10bc26fd7f7f3ca9c29b9d8d09594c046
AshishBoora1 <boora8837@gmail.com>
2025-08-14 17:17:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import FormComp from "../Components/FormComp";
+
+function App() {
+
+
+
+  return (
+    <div>
+      <FormComp/>
+    </div>
+  );
+}
+
+export default App;
